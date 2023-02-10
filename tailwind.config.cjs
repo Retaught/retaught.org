549b750f4306935cde85c474dab9d26092a81d34
Cosmeak <guillaumefine09@gmail.com>
2023-02-10 21:55:57 +0100

    :sparkles: FEAT rubik font and base lib folder

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3F9BFF',
+        secondary: '#5856D6',
+        gray: '#27274D',
+        black: '#0C0F18',
+        white: '#F4F7FF',
+        danger: '#FF3B30',
+        warning: '#FF9500',
+        success: '#34C759',
+      },
+      fontFamily: {
+        rubik: 'Rubik'
+      }
+    },
   },
   plugins: [],
 }
